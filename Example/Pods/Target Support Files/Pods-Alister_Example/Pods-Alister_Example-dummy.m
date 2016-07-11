ce312a3a4801a44e913b244112b5dd1f36d41666
Dmitriy Frolov <dmitriy.frolov@anoda.mobi>
2016-07-11 17:00:02 +0300

    cleanup pods

--- Example/Pods/Target Support Files/Pods-Alister_Example/Pods-Alister_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Alister_Example : NSObject
-@end
-@implementation PodsDummy_Pods_Alister_Example
-@end
--- Example/Pods/Target Support Files/Pods-Alister_Tests/Pods-Alister_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Alister_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_Alister_Tests
-@end
