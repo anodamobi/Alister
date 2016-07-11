51dec8f8aa71b3747c897743a1583c6557f22666
Dmitriy Frolov <dmitriy.frolov@anoda.mobi>
2016-07-11 15:35:12 +0300

    storage update model test were added

--- Example/Tests/ANStorageUpdateModelTest.m
+++ Example/Tests/ANStorageUpdateModelTest.m
@@ -32,7 +32,11 @@
 
 - (void)test_conformsToProtocol_positive
 {
-
+    //when
+    BOOL conformsToProtocol = [self.model conformsToProtocol:@protocol(ANStorageUpdateModelInterface)];
+    
+    //then
+    XCTAssertTrue(conformsToProtocol);
 }
 
 //@property (nonatomic, assign) BOOL isRequireReload;
@@ -42,17 +46,31 @@
 
 - (void)test_isEmpty_positive_emptyWhenCreated
 {
-	
+    //given
+    ANStorageUpdateModel* testModel = [ANStorageUpdateModel new];
+   
+    //then
+    XCTAssertTrue([testModel isEmpty]);
 }
 
 - (void)test_isEmpty_positive_notEmptyWhenAddItem
 {
+    //given
+    [self.model mergeWith:[self _fullTestModel]];
     
+    //when
+    
+    //then
+    XCTAssertFalse([self.model isEmpty]);
 }
 
 - (void)test_isEmpty_positive_notEmptyWhenNeedsReload
 {
-    
+//    [self.model mergeWith:[self _fullTestModel]];
+//    [self.model setIsRequireReload:YES];
+//    
+//    BOOL isEmpty = [self.model isEmpty];
+//    NSLog(@"");
 }
 
 
@@ -60,7 +78,11 @@
 
 - (void)test_isRequireReload_positive_falseWhenCreated
 {
-
+    //given
+    ANStorageUpdateModel*  testModel = [ANStorageUpdateModel new];
+    
+    //then
+    XCTAssertFalse(testModel.isRequireReload);
 }
 
 - (void)test_isRequireReload_positive_falseWhenUpdateWithModelWithFalseAndNoUpdates
@@ -81,8 +103,32 @@
 
 - (void)test_mergeWith_positive // TODO: add more conditions
 {
-	
+    
+    //given
+    XCTAssertTrue([self.model isEmpty]);
+    
+    //when
+    [self.model mergeWith:[self _fullTestModel]];
+    
+    //then
+    XCTAssertFalse([self.model isEmpty]);
 }
 
+
+- (id<ANStorageUpdateModelInterface>)_fullTestModel
+{
+    ANStorageUpdateModel* testModel = [ANStorageUpdateModel new];
+    
+    [testModel addInsertedSectionIndexes:[NSIndexSet indexSetWithIndex:1]];
+    [testModel addUpdatedSectionIndexes:[NSIndexSet indexSetWithIndex:1]];
+    [testModel addDeletedSectionIndexes:[NSIndexSet indexSetWithIndex:1]];
+    
+    [testModel addInsertedIndexPaths:@[[NSIndexPath indexPathWithIndex:0]]];
+    [testModel addUpdatedIndexPaths:@[[NSIndexPath indexPathWithIndex:0]]];
+    [testModel addDeletedIndexPaths:@[[NSIndexPath indexPathWithIndex:0]]];
+    [testModel addMovedIndexPaths:@[[NSIndexPath indexPathWithIndex:0]]];
+    
+    return testModel;
+}
 
 @end
