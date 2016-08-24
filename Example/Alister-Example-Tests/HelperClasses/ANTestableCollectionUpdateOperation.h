ec66c3821c1ed97a2aa0e83a4030722d6a8aa964
Oksana Kovalchuk <oksana@anoda.mobi>
2016-08-24 16:40:40 +0300

    created testable collection update operation

---
+++ Example/Alister-Example-Tests/HelperClasses/ANTestableCollectionUpdateOperation.h
@@ -0,0 +1,13 @@
+//
+//  ANTestableCollectionUpdateOperation.h
+//  Alister-Example
+//
+//  Created by Oksana Kovalchuk on 8/24/16.
+//  Copyright © 2016 Oksana Kovalchuk. All rights reserved.
+//
+
+#import <Alister/Alister.h>
+
+@interface ANTestableCollectionUpdateOperation : ANCollectionControllerUpdateOperation
+
+@end
---
+++ Example/Alister-Example-Tests/HelperClasses/ANTestableCollectionUpdateOperation.m
@@ -0,0 +1,13 @@
+//
+//  ANTestableCollectionUpdateOperation.m
+//  Alister-Example
+//
+//  Created by Oksana Kovalchuk on 8/24/16.
+//  Copyright © 2016 Oksana Kovalchuk. All rights reserved.
+//
+
+#import "ANTestableCollectionUpdateOperation.h"
+
+@implementation ANTestableCollectionUpdateOperation
+
+@end
