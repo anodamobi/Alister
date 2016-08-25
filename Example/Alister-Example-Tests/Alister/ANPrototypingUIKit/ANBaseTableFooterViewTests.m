e564211a83a7076e04ac6e6c006b6e56f78da748
Maxim Eremenko <maxim.eremenko@anoda.mobi>
2016-08-25 13:30:36 +0300

    ANBaseTableFooterViewTests was added

---
+++ Example/Alister-Example-Tests/Alister/ANPrototypingUIKit/ANBaseTableFooterViewTests.m
@@ -0,0 +1,41 @@
+//
+//  ANBaseTableFooterViewTests.m
+//  Alister-Example
+//
+//  Created by Maxim Eremenko on 8/22/16.
+//  Copyright © 2016 Oksana Kovalchuk. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import <Expecta/Expecta.h>
+#import "ANBaseTableFooterView.h"
+
+static BOOL const kIsTransparent = YES;
+
+@interface ANBaseTableFooterViewTests : XCTestCase
+
+@property (nonatomic, strong) ANBaseTableFooterView* view;
+
+@end
+
+@implementation ANBaseTableFooterViewTests
+
+- (void)setUp
+{
+    [super setUp];
+    self.view = [ANBaseTableFooterView new];
+    self.view.isTransparent = kIsTransparent;
+}
+
+- (void)tearDown
+{
+    self.view = nil;
+    [super tearDown];
+}
+
+- (void)test_isTransparent_positive
+{
+    expect(self.view.isTransparent).equal(kIsTransparent);
+}
+
+@end
