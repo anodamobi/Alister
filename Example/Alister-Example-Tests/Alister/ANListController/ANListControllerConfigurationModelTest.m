51b1037cd82784229ae513dd72febfe77d93678f
Maxim Eremenko <maxim.eremenko@anoda.mobi>
2016-08-25 17:09:18 +0300

    initial commit

---
+++ Example/Alister-Example-Tests/Alister/ANListController/ANListControllerConfigurationModelTest.m
@@ -0,0 +1,39 @@
+//
+//  ANListControllerConfigurationModelTest.m
+//  Alister-Example
+//
+//  Created by ANODA on 8/25/16.
+//  Copyright © 2016 Oksana Kovalchuk. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ANListControllerConfigurationModelTest : XCTestCase
+
+@end
+
+@implementation ANListControllerConfigurationModelTest
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
