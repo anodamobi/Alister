5b19d696ddf3a3d596c075f67ef36975169df2ef
Maxim Eremenko <maxim.eremenko@anoda.mobi>
2016-08-25 17:11:33 +0300

    ANListControllerConfigurationModelTest added

--- Example/Alister-Example-Tests/Alister/ANListController/ANListControllerConfigurationModelTest.m
+++ Example/Alister-Example-Tests/Alister/ANListController/ANListControllerConfigurationModelTest.m
@@ -7,33 +7,145 @@
 //
 
 #import <XCTest/XCTest.h>
+#import <Expecta/Expecta.h>
+#import "ANListControllerConfigurationModel.h"
+
+static BOOL const kShouldHandleKeyboard = YES;
+static CGFloat const kReloadAnimationDuration = 1.2f;
+static CGFloat const kDefaultAnimationDuration = 2.3f;
+static NSString* const kReloadAnimationKey = @"kReloadAnimationKey";
+
+static BOOL const kShouldDisplayHeaderOnEmptySection = YES;
+static BOOL const kShouldDisplayFooterOnEmptySection = YES;
+
+static UITableViewRowAnimation const kInsertSectionAnimation = UITableViewRowAnimationMiddle;
+static UITableViewRowAnimation const kDeleteSectionAnimation = UITableViewRowAnimationRight;
+static UITableViewRowAnimation const kReloadSectionAnimation = UITableViewRowAnimationLeft;
+
+static UITableViewRowAnimation const kInsertRowAnimation = UITableViewRowAnimationMiddle;
+static UITableViewRowAnimation const kDeleteRowAnimation = UITableViewRowAnimationRight;
+static UITableViewRowAnimation const kReloadRowAnimation = UITableViewRowAnimationLeft;
+
+static NSString* const kDefaultHeaderSupplementary = @"kDefaultHeaderSupplementary";
+static NSString* const kDefaultFooterSupplementary = @"kDefaultFooterSupplementary";
 
 @interface ANListControllerConfigurationModelTest : XCTestCase
+
+@property (nonatomic, strong) ANListControllerConfigurationModel* model;
 
 @end
 
 @implementation ANListControllerConfigurationModelTest
 
-- (void)setUp {
+- (void)setUp
+{
     [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
+    self.model = [ANListControllerConfigurationModel new];
 }
 
-- (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
+- (void)tearDown
+{
+    self.model = nil;
     [super tearDown];
 }
 
-- (void)testExample {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
+- (ANListControllerConfigurationModel*)_fullModel
+{
+    ANListControllerConfigurationModel* model = [ANListControllerConfigurationModel new];
+    
+    model.shouldHandleKeyboard = kShouldHandleKeyboard;
+    model.reloadAnimationDuration = kReloadAnimationDuration;
+    model.reloadAnimationKey = kReloadAnimationKey;
+    model.shouldDisplayHeaderOnEmptySection = kShouldDisplayHeaderOnEmptySection;
+    model.shouldDisplayFooterOnEmptySection = kShouldDisplayFooterOnEmptySection;
+    model.insertSectionAnimation = kInsertSectionAnimation;
+    model.deleteSectionAnimation = kDeleteSectionAnimation;
+    model.reloadSectionAnimation = kReloadSectionAnimation;
+    
+    model.insertRowAnimation = kInsertRowAnimation;
+    model.deleteRowAnimation = kDeleteRowAnimation;
+    model.reloadRowAnimation = kReloadRowAnimation;
+    
+    model.defaultHeaderSupplementary = kDefaultHeaderSupplementary;
+    model.defaultFooterSupplementary = kDefaultFooterSupplementary;
+    model.defaultAnimationDuration = kDefaultAnimationDuration;
+    
+    return model;
 }
 
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
+
+#pragma mark - setters
+
+- (void)test_setters_positive_propertiesIsSetRight
+{
+    // given
+    ANListControllerConfigurationModel* model = [self _fullModel];
+    
+    // then
+    expect(model.shouldHandleKeyboard).equal(kShouldHandleKeyboard);
+    expect(model.reloadAnimationDuration).equal(kReloadAnimationDuration);
+    expect(model.reloadAnimationKey).equal(kReloadAnimationKey);
+    expect(model.shouldDisplayHeaderOnEmptySection).equal(kShouldDisplayHeaderOnEmptySection);
+    expect(model.shouldDisplayFooterOnEmptySection).equal(kShouldDisplayFooterOnEmptySection);
+    expect(model.insertSectionAnimation).equal(kInsertSectionAnimation);
+    expect(model.deleteSectionAnimation).equal(kDeleteSectionAnimation);
+    expect(model.reloadSectionAnimation).equal(kReloadSectionAnimation);
+    
+    expect(model.insertRowAnimation).equal(kInsertRowAnimation);
+    expect(model.deleteRowAnimation).equal(kDeleteRowAnimation);
+    expect(model.reloadRowAnimation).equal(kReloadRowAnimation);
+    
+    expect(model.defaultHeaderSupplementary).equal(kDefaultHeaderSupplementary);
+    expect(model.defaultFooterSupplementary).equal(kDefaultFooterSupplementary);
+    expect(model.defaultAnimationDuration).equal(kDefaultAnimationDuration);
+}
+
+
+#pragma mark - ANListControllerConfigurationModelInterface
+
+- (void)test_protocolConformation_positive_modelConformsToModelInterface
+{
+    expect(self.model).to.conformTo(@protocol(ANListControllerConfigurationModelInterface));
+}
+
+- (void)test_shouldHandleKeyboard_positive_modelRespondsToSelector
+{
+    expect(self.model).respondTo(@selector(shouldHandleKeyboard));
+}
+
+- (void)test_reloadAnimationDuration_positive_modelRespondsToSelector
+{
+    expect(self.model).respondTo(@selector(reloadAnimationDuration));
+}
+
+- (void)test_reloadAnimationKey_positive_modelRespondsToSelector
+{
+    expect(self.model).respondTo(@selector(reloadAnimationKey));
+}
+
+- (void)test_setReloadAnimationKey_positive_modelRespondsToSelector
+{
+    expect(self.model).respondTo(@selector(setReloadAnimationKey:));
+}
+
+- (void)test_defaultHeaderSupplementary_positive_modelRespondsToSelector
+{
+    expect(self.model).respondTo(@selector(defaultHeaderSupplementary));
+}
+
+- (void)test_defaultFooterSupplementary_positive_modelRespondsToSelector
+{
+    expect(self.model).respondTo(@selector(defaultHeaderSupplementary));
+}
+
+- (void)test_setDefaultHeaderSupplementary_positive_modelRespondsToSelector
+{
+    expect(self.model).respondTo(@selector(setDefaultHeaderSupplementary:));
+}
+
+- (void)test_setDefaultFooterSupplementary_positive_modelRespondsToSelector
+{
+    expect(self.model).respondTo(@selector(setDefaultFooterSupplementary:));
 }
 
 @end
