17d8e105c8c776161b5dfa475ea8a2ad02efcd83
Oksana Kovalchuk <oksana@anoda.mobi>
2016-09-06 20:46:03 +0300

    fix PR comments

--- Example/Alister-Example-Tests/Alister/ANStorageUpdateOperationTests/ANStorageUpdateOperationTests.m
+++ Example/Alister-Example-Tests/Alister/ANStorageUpdateOperationTests/ANStorageUpdateOperationTests.m
@@ -34,12 +34,10 @@
 - (void)setUp
 {
     [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
 }
 
 - (void)tearDown
 {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
     [super tearDown];
 }
 
@@ -56,7 +54,7 @@
 - (void)test_updaterDelegate_positive_isNotEmptyAfterAssign
 {
     //given
-     ANStorageUpdateOperation* updateOperation = [ANStorageUpdateOperation operationWithExecutionBlock:nil];
+    ANStorageUpdateOperation* updateOperation = [ANStorageUpdateOperation operationWithExecutionBlock:nil];
     updateOperation.updaterDelegate = self;
     
     //then
@@ -83,7 +81,6 @@
     ANStorageUpdateOperationConfigurationBlock configBlock = ^(ANStorageUpdateOperation* updateOperation){
         [expectation fulfill];
         XCTAssertTrue([operation isEqual:updateOperation]);
-        
     };
     
     operation.configurationBlock = configBlock;
