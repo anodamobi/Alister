bbf813a25c71c999175d2ec4b1e9f0def52bfde1
Oksana Kovalchuk <oksana@anoda.mobi>
2016-10-04 13:30:06 +0300

    update skicky footer for ios 10

--- Alister/ANPrototypingUIKit/TableView/ANTableView.m
+++ Alister/ANPrototypingUIKit/TableView/ANTableView.m
@@ -13,13 +13,25 @@ static CGFloat const kDefaultTableViewHeaderHeight = 40;
 #define SYSTEM_VERSION          ([[[UIDevice currentDevice] systemVersion] floatValue])
 #define IOS7_OR_HIGHER          (7.0 <= SYSTEM_VERSION)
 
+#import "Masonry.h"
+#import <FrameAccessor/FrameAccessor.h>
+
 @interface ANTableView ()
 
 @property (nonatomic, strong) UIView* stickedContainer;
+@property (nonatomic, assign) CGFloat stickedFooterHeight;
+@property (nonatomic, strong) UIView* bottomStickedFooterView;
 
 @end
 
 @implementation ANTableView
+
++ (instancetype)cleanTableWithFrame:(CGRect)frame style:(UITableViewStyle)style
+{
+    ANTableView* table = [[[self class] alloc] initWithFrame:frame style:style];
+    [table setupAppearance];
+    return table;
+}
 
 - (instancetype)initWithFrame:(CGRect)frame style:(UITableViewStyle)style
 {
@@ -35,32 +47,30 @@ static CGFloat const kDefaultTableViewHeaderHeight = 40;
     return self;
 }
 
-+ (instancetype)cleanTableWithFrame:(CGRect)frame style:(UITableViewStyle)style
+- (void)addStickyFooter:(UIView*)footer withFixedHeight:(CGFloat)height
 {
-    ANTableView* table = [[[self class] alloc] initWithFrame:frame style:style];
-    [table setupAppearance];
-    return table;
+    self.stickedFooterHeight = height;
+    self.bottomStickedFooterView = footer;
 }
 
 - (void)layoutSubviews
 {
     [super layoutSubviews];
+    [self layoutTableFooterView];
     
     CGRect frame = self.bottomStickedFooterView.frame;
-    frame.origin.y = self.stickedContainer.frame.size.height - frame.size.height;
-    frame.size.width = self.frame.size.width;
-    
+    frame.size.height = self.stickedFooterHeight;
+    frame.size.width = self.width;
+    frame.origin.y = self.stickedContainer.height - frame.size.height;
     self.bottomStickedFooterView.frame = frame;
-    
-    [self layoutTableFooterView];
 }
 
 - (void)layoutTableFooterView
 {
-    if (self.bottomStickedFooterView)
+    if (self.bottomStickedFooterView && self.contentSize.height)
     {
         CGFloat contentSizeWithoutFooter = self.tableFooterView.frame.origin.y;
-        CGFloat stickedFooterHeight = self.bottomStickedFooterView.frame.size.height;
+        CGFloat stickedFooterHeight = self.stickedFooterHeight;
         CGFloat frameHeight = self.frame.size.height;
         
         CGFloat minContentHeightWithFooter = contentSizeWithoutFooter + stickedFooterHeight;
