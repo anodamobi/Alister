3c8db9068966edd1a8262ab3883b5797dc313c28
Oksana Kovalchuk <oksana@anoda.mobi>
2016-10-14 20:46:22 +0300

    📝added doc for ANStorageSectionModel

--- Alister/ANStorage/Models/Section/ANStorageSectionModel.h
+++ Alister/ANStorage/Models/Section/ANStorageSectionModel.h
@@ -8,14 +8,57 @@
 
 #import "ANStorageSectionModelInterface.h"
 
+/**
+ Private class to store section objects in memory.
+ You should not call this call directly.
+ */
 @interface ANStorageSectionModel : NSObject <ANStorageSectionModelInterface>
 
+
+/**
+ Array of all objects in this section
+ */
 @property (nonatomic, strong, readonly, nonnull) NSArray* objects;
+
+
+/**
+ Dictionary with supplementaries (headers, footers) models and kinds
+ */
 @property (nonatomic, strong, readonly, nonnull) NSDictionary* supplementaryObjects;
 
-- (void)addItem:(_Nonnull id)item;
-- (void)insertItem:(_Nonnull id)item atIndex:(NSUInteger)index;
+
+/**
+ Adds new item in the end of objects array
+
+ @param item to add. If item will be nil no expection will be generated.
+ */
+- (void)addItem:(nonnull id)item;
+
+
+/**
+ Inserts item at the specified index. If index is out of bounds or item is nil operation will skipped.
+
+ @param item  item to add in storage
+ @param index for item in the existing objects array
+ */
+- (void)insertItem:(nonnull id)item atIndex:(NSUInteger)index;
+
+
+/**
+ Removes item at specified index. If section not exist n
+
+ @param index section index to remove
+ */
 - (void)removeItemAtIndex:(NSUInteger)index;
+
+
+/**
+ Replaces item at specified index on a new item. 
+ If item at specified index not esists or new item is nil operation will be skipped.
+
+ @param index index for item to replace
+ @param item  new item to insert
+ */
 - (void)replaceItemAtIndex:(NSUInteger)index withItem:(_Nonnull id)item;
 
 @end
