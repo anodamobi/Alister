33afd9e6612e50a3d4ed8387961a79625c314b2a
Oksana Kovalchuk <oksana@anoda.mobi>
2016-10-14 20:59:51 +0300

    📝added doc for ANStorageUpdateModel

--- Alister/ANStorage/Models/Update/ANStorageUpdateModel.h
+++ Alister/ANStorage/Models/Update/ANStorageUpdateModel.h
@@ -10,11 +10,34 @@
 #import "ANStorageMovedIndexPathModel.h"
 #import "ANBaseDomainModel.h" // TODO: temp for debug
 
+
+/**
+ Private class represents any update transaction related to ANStorageModel.
+ Never should be used directly.
+ */
 @interface ANStorageUpdateModel : ANBaseDomainModel <ANStorageUpdateModelInterface>
 
+
+/**
+ Indicates does storage should reload related TableView or CollectionView or try to execute update
+ */
 @property (nonatomic, assign) BOOL isRequireReload;
 
+
+/**
+ Indicates does current model contain any changes
+
+ @return BOOL state
+ */
 - (BOOL)isEmpty;
+
+
+/**
+ Merges one updateModel with another. 
+ Object that was executor for this operation will get all updates from another model.
+
+ @param model model to merge all updates with
+ */
 - (void)mergeWith:(id<ANStorageUpdateModelInterface>)model;
 
 @end
