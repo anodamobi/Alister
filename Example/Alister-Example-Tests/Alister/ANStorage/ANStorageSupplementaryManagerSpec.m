bfef77cafd702ba7bf8ed1331dfd2afcf711bde8
Oksana Kovalchuk <oksana@anoda.mobi>
2016-10-18 00:46:59 +0300

    ✅finished tests for ANStorageSupplementaryManager

--- Example/Alister-Example-Tests/Alister/ANStorage/ANStorageSupplementaryManagerSpec.m
+++ Example/Alister-Example-Tests/Alister/ANStorage/ANStorageSupplementaryManagerSpec.m
@@ -21,6 +21,73 @@ beforeEach(^{
 
 describe(@"updateSectionHeaderModel: forSectionIndex: inStorage:", ^{
     
+    beforeEach(^{
+        storage.headerKind = @"testKind";
+    });
+    
+    it(@"updates model successfully", ^{
+        
+        NSString* item = @"test";
+        [ANStorageSupplementaryManager updateSectionHeaderModel:item
+                                                forSectionIndex:0
+                                                      inStorage:storage];
+        
+        expect([[storage sectionAtIndex:0] supplementaryModelOfKind:storage.headerKind]).equal(item);
+    });
+    
+    it(@"no assert if model is nil", ^{
+        void(^block)() = ^() {
+            [ANStorageSupplementaryManager updateSectionHeaderModel:nil
+                                                    forSectionIndex:0
+                                                          inStorage:storage];
+        };
+        expect(block).notTo.raiseAny();
+    });
+    
+    it(@"no assert if index is out of bounds", ^{
+        void(^block)() = ^() {
+            [ANStorageSupplementaryManager updateSectionHeaderModel:@"test"
+                                                    forSectionIndex:10
+                                                          inStorage:storage];
+        };
+        expect(block).notTo.raiseAny();
+    });
+    
+    it(@"no assert if index is negative", ^{
+        void(^block)() = ^() {
+            [ANStorageSupplementaryManager updateSectionHeaderModel:@"test"
+                                                    forSectionIndex:-1
+                                                          inStorage:storage];
+        };
+        expect(block).notTo.raiseAny();
+    });
+    
+    it(@"no assert if index is NSNotFound", ^{
+        void(^block)() = ^() {
+            [ANStorageSupplementaryManager updateSectionHeaderModel:@"test"
+                                                    forSectionIndex:NSNotFound
+                                                          inStorage:storage];
+        };
+        expect(block).notTo.raiseAny();
+    });
+    
+    it(@"no assert if storage is nil", ^{
+        void(^block)() = ^() {
+            [ANStorageSupplementaryManager updateSectionHeaderModel:@"test"
+                                                    forSectionIndex:0
+                                                          inStorage:nil];
+        };
+        expect(block).notTo.raiseAny();
+    });
+});
+
+
+describe(@"updateSectionFooterModel: forSectionIndex: inStorage:", ^{
+   
+    beforeEach(^{
+        storage.footerKind = @"testKind";
+    });
+    
     it(@"updates model successfully", ^{
         
         NSString* item = @"test";
@@ -78,18 +145,68 @@ describe(@"updateSectionHeaderModel: forSectionIndex: inStorage:", ^{
 });
 
 
-describe(@"updateSectionFooterModel: forSectionIndex: inStorage:", ^{
-   
-    it(@"no assert if model is nil", ^{
-        failure(@"Pending");
-    });
-});
-
-
 describe(@"supplementaryModelOfKind: forSectionIndex: inStorage:", ^{
     
-    it(@"no assert if model is nil", ^{
-        failure(@"Pending");
+    __block NSString* kind = @"testKind";
+    __block NSString* item = @"item";
+    
+    beforeEach(^{
+        storage.footerKind = kind;
+        ANStorageSectionModel* section = [ANStorageSectionModel new];
+        [section updateSupplementaryModel:item forKind:kind];
+        [storage addSection:section];
+    });
+    
+    it(@"returns successfully", ^{
+        id model = [ANStorageSupplementaryManager supplementaryModelOfKind:kind
+                                                           forSectionIndex:0
+                                                                 inStorage:storage];
+        expect(model).equal(item);
+    });
+    
+    it(@"no assert if kind is nil", ^{
+        void(^block)() = ^() {
+            [ANStorageSupplementaryManager supplementaryModelOfKind:nil
+                                                    forSectionIndex:0
+                                                          inStorage:storage];
+        };
+        expect(block).notTo.raiseAny();
+    });
+    
+    it(@"no assert if index is out of bounds", ^{
+        void(^block)() = ^() {
+            [ANStorageSupplementaryManager supplementaryModelOfKind:kind
+                                                    forSectionIndex:5
+                                                          inStorage:storage];
+        };
+        expect(block).notTo.raiseAny();
+    });
+    
+    it(@"no assert if index is negative", ^{
+        void(^block)() = ^() {
+            [ANStorageSupplementaryManager supplementaryModelOfKind:kind
+                                                    forSectionIndex:-1
+                                                          inStorage:storage];
+        };
+        expect(block).notTo.raiseAny();
+    });
+    
+    it(@"no assert if index is NSNotFound", ^{
+        void(^block)() = ^() {
+            [ANStorageSupplementaryManager supplementaryModelOfKind:kind
+                                                    forSectionIndex:NSNotFound
+                                                          inStorage:storage];
+        };
+        expect(block).notTo.raiseAny();
+    });
+    
+    it(@"no assert if storage is nil", ^{
+        void(^block)() = ^() {
+            [ANStorageSupplementaryManager supplementaryModelOfKind:kind
+                                                    forSectionIndex:0
+                                                          inStorage:nil];
+        };
+        expect(block).notTo.raiseAny();
     });
 });
 
--- Example/Alister-Example-Tests/Alister/ANStorage/ANStorageSupplementaryManager_UpdateVerification_Spec.m
+++ Example/Alister-Example-Tests/Alister/ANStorage/ANStorageSupplementaryManager_UpdateVerification_Spec.m
@@ -6,43 +6,134 @@
 //  Copyright © 2016 Oksana Kovalchuk. All rights reserved.
 //
 
+#import <Alister/ANStorageSupplementaryManager.h>
+#import <Alister/ANStorageModel.h>
+#import <Alister/ANStorageSectionModel.h>
+#import <Alister/ANStorageUpdateModel.h>
 
-SpecBegin()
+SpecBegin(ANStorageSupplementaryManager_UpdateVerification)
+
+__block ANStorageModel* storage = nil;
 
 beforeEach(^{
-    
+    storage = [ANStorageModel new];
 });
 
 describe(@"updateSectionHeaderModel: forSectionIndex: inStorage:", ^{
     
-    it(@"no assert if model is nil", ^{
-        failure(@"Pending");
+    beforeEach(^{
+        storage.headerKind = @"testKind";
+    });
+    
+    it(@"updates model successfully", ^{
+        
+        NSString* item = @"test";
+        ANStorageUpdateModel* update = [ANStorageSupplementaryManager updateSectionHeaderModel:item
+                                                                               forSectionIndex:0
+                                                                                     inStorage:storage];
+        ANStorageUpdateModel* expected = [ANStorageUpdateModel new];
+        [expected addInsertedSectionIndex:0];
+        
+        expect(update).equal(expected);
+    });
+    
+    it(@"no update will be generated if model is nil", ^{
+        ANStorageUpdateModel* update = [ANStorageSupplementaryManager updateSectionHeaderModel:nil
+                                                                               forSectionIndex:0
+                                                                                     inStorage:storage];
+        expect(update.isEmpty).beTruthy();
+    });
+    
+    it(@"successfully generated update if section is not exist", ^{
+        ANStorageUpdateModel* update = [ANStorageSupplementaryManager updateSectionHeaderModel:@"test"
+                                                                               forSectionIndex:1
+                                                                                     inStorage:storage];
+        ANStorageUpdateModel* expected = [ANStorageUpdateModel new];
+        [expected addInsertedSectionIndexes:[NSIndexSet indexSetWithIndexesInRange:NSMakeRange(0, 2)]];
+        
+        expect(update).equal(expected);
+    });
+    
+    it(@"no update will be generated if index is negative", ^{
+        ANStorageUpdateModel* update = [ANStorageSupplementaryManager updateSectionHeaderModel:@"test"
+                                                                               forSectionIndex:-1
+                                                                                     inStorage:storage];
+        expect(update.isEmpty).beTruthy();
+    });
+    
+    it(@"no update will be generated if index is NSNotFound", ^{
+        ANStorageUpdateModel* update = [ANStorageSupplementaryManager updateSectionHeaderModel:@"test"
+                                                                               forSectionIndex:NSNotFound
+                                                                                     inStorage:storage];
+        expect(update.isEmpty).beTruthy();
+    });
+    
+    it(@"no update will be generated if storage is nil", ^{
+        ANStorageUpdateModel* update = [ANStorageSupplementaryManager updateSectionHeaderModel:@"test"
+                                                                               forSectionIndex:0
+                                                                                     inStorage:nil];
+        expect(update.isEmpty).beTruthy();
     });
 });
 
 
 describe(@"updateSectionFooterModel: forSectionIndex: inStorage:", ^{
     
-    it(@"no assert if model is nil", ^{
-        failure(@"Pending");
+    beforeEach(^{
+        storage.footerKind = @"testKind";
     });
-});
-
-
-describe(@"supplementaryModelOfKind: forSectionIndex: inStorage:", ^{
     
-    it(@"no assert if model is nil", ^{
-        failure(@"Pending");
+    it(@"updates model successfully", ^{
+        
+        NSString* item = @"test";
+        ANStorageUpdateModel* update = [ANStorageSupplementaryManager updateSectionFooterModel:item
+                                                forSectionIndex:0
+                                                      inStorage:storage];
+        
+        ANStorageUpdateModel* expected = [ANStorageUpdateModel new];
+        [expected addInsertedSectionIndex:0];
+        
+        expect(update).equal(expected);
+    });
+    
+    it(@"successfully generated update if section is not exist", ^{
+        ANStorageUpdateModel* update = [ANStorageSupplementaryManager updateSectionFooterModel:@"test"
+                                                                               forSectionIndex:1
+                                                                                     inStorage:storage];
+        ANStorageUpdateModel* expected = [ANStorageUpdateModel new];
+        [expected addInsertedSectionIndexes:[NSIndexSet indexSetWithIndexesInRange:NSMakeRange(0, 2)]];
+        
+        expect(update).equal(expected);
+    });
+    
+    it(@"no update will be generated if model is nil", ^{
+        ANStorageUpdateModel* update = [ANStorageSupplementaryManager updateSectionFooterModel:nil
+                                                                               forSectionIndex:0
+                                                                                     inStorage:storage];
+        expect(update.isEmpty).beTruthy();
+    });
+    
+    it(@"no update will be generated if index is negative", ^{
+        ANStorageUpdateModel* update = [ANStorageSupplementaryManager updateSectionFooterModel:@"test"
+                                                                               forSectionIndex:-1
+                                                                                     inStorage:storage];
+        expect(update.isEmpty).beTruthy();
+    });
+    
+    it(@"no update will be generated if index is NSNotFound", ^{
+        ANStorageUpdateModel* update = [ANStorageSupplementaryManager updateSectionFooterModel:@"test"
+                                                                               forSectionIndex:NSNotFound
+                                                                                     inStorage:storage];
+        expect(update.isEmpty).beTruthy();
+    });
+    
+    it(@"no update will be generated if storage is nil", ^{
+        ANStorageUpdateModel* update = [ANStorageSupplementaryManager updateSectionFooterModel:@"test"
+                                                                               forSectionIndex:0
+                                                                                     inStorage:nil];
+        expect(update.isEmpty).beTruthy();
     });
 });
 
 SpecEnd
 
-
-//+ (ANStorageUpdateModel*)updateSectionHeaderModel:(id)headerModel
-//forSectionIndex:(NSInteger)sectionIndex
-//inStorage:(ANStorageModel*)storage;
-//
-//+ (ANStorageUpdateModel*)updateSectionFooterModel:(id)footerModel
-//forSectionIndex:(NSUInteger)sectionIndex
-//inStorage:(ANStorageModel*)storage;
