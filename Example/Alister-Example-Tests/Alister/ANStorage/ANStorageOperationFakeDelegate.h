a8040a862b3a4454dc0c8a8552ce91973f4ddd98
Oksana Kovalchuk <oksana@anoda.mobi>
2016-10-18 04:22:50 +0300

    ✅ added tests for ANStorageUpdateOperation

---
+++ Example/Alister-Example-Tests/Alister/ANStorage/ANStorageOperationFakeDelegate.h
@@ -0,0 +1,13 @@
+//
+//  ANStorageOperationFakeDelegate.h
+//  Alister-Example
+//
+//  Created by Oksana Kovalchuk on 10/18/16.
+//  Copyright © 2016 Oksana Kovalchuk. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ANStorageOperationFakeDelegate : NSObject
+
+@end
---
+++ Example/Alister-Example-Tests/Alister/ANStorage/ANStorageOperationFakeDelegate.m
@@ -0,0 +1,13 @@
+//
+//  ANStorageOperationFakeDelegate.m
+//  Alister-Example
+//
+//  Created by Oksana Kovalchuk on 10/18/16.
+//  Copyright © 2016 Oksana Kovalchuk. All rights reserved.
+//
+
+#import "ANStorageOperationFakeDelegate.h"
+
+@implementation ANStorageOperationFakeDelegate
+
+@end
