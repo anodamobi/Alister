70a871a081afa398a7d20e90a0afcd855c90fc91
Oleg Panfyorov <oleg.panfyorov@anoda.mobi>
2016-10-20 18:34:35 +0300

    💄 Added BottomStickedFooter folder with init tests

---
+++ Example/Alister-Example-Tests/ExampleTests/ControllersTests/BottomStickedFooter/ANBottomStickedFooterVCSpec.m
@@ -0,0 +1,30 @@
+//
+//  ANBottomStickedFooterVCSpec.m
+//  Alister-Example
+//
+//  Created by ANODA on 10/20/16.
+//  Copyright © 2016 Oksana Kovalchuk. All rights reserved.
+//
+
+#import "ANEBottomStickedFooterVC.h"
+
+SpecBegin(ANBottomStickedFooterVC)
+
+describe(@"ANEBottomStickedFooterVC", ^{
+    
+    __block ANEBottomStickedFooterVC* footerVC = [ANEBottomStickedFooterVC new];
+    
+    beforeEach(^{
+
+    });
+    
+    it(@"should do stuff", ^{
+
+    });
+    
+    afterEach(^{
+        footerVC = nil;
+    });
+});
+
+SpecEnd
