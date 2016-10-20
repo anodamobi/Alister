5b1c2a8d9b9da57679e3f4787a5ebb07ddffc0fc
Oleg Panfyorov <oleg.panfyorov@anoda.mobi>
2016-10-20 15:08:02 +0300

    💄added reordering vc and controller

---
+++ Example/Alister-Example/ViewControllers/Reordering/ANEReorderingController.m
@@ -0,0 +1,64 @@
+//
+//  ANEReorderingController.m
+//  Alister-Example
+//
+//  Created by ANODA on 10/20/16.
+//  Copyright © 2016 Oksana Kovalchuk. All rights reserved.
+//
+
+#import "ANEReorderingController.h"
+#import "ANELabelTableViewCell.h"
+#import "ANBaseTableHeaderView.h"
+
+@implementation ANEReorderingController
+
+- (instancetype)initWithTableView:(UITableView*)tableView
+{
+    self = [super initWithTableView:tableView];
+    if (self)
+    {
+        [self configureCellsWithBlock:^(id<ANListControllerReusableInterface> configurator) {
+            
+            [configurator registerCellClass:[ANELabelTableViewCell class]
+                             forSystemClass:[NSString class]];
+            
+            [configurator registerHeaderClass:[ANBaseTableHeaderView class]
+                               forSystemClass:[NSString class]];
+        }];
+    }
+    
+    return self;
+}
+
+- (void)updateWithStorage:(ANStorage*)storage;
+{
+    [self attachStorage:storage];
+}
+
+
+#pragma mark - UITableViewDelegate
+
+- (void)tableView:(UITableView*)__unused tableView moveRowAtIndexPath:(NSIndexPath*)__unused sourceIndexPath
+                                                          toIndexPath:(NSIndexPath*)__unused destinationIndexPath
+{
+    [self.currentStorage updateWithoutAnimationChangeBlock:^(id<ANStorageUpdatableInterface> storageController) {
+        [storageController moveItemFromIndexPath:sourceIndexPath toIndexPath:destinationIndexPath];
+    }];
+}
+
+- (UITableViewCellEditingStyle)tableView:(UITableView*)__unused tableView editingStyleForRowAtIndexPath:(NSIndexPath*)__unused indexPath
+{
+    return UITableViewCellEditingStyleNone;
+}
+
+- (BOOL)tableView:(UITableView*)__unused tableview shouldIndentWhileEditingRowAtIndexPath:(NSIndexPath*)__unused indexPath
+{
+    return NO;
+}
+
+- (BOOL)tableView:(UITableView*)__unused tableview canMoveRowAtIndexPath:(NSIndexPath*)__unused indexPath
+{
+    return YES;
+}
+
+@end
---
+++ Example/Alister-Example/ViewControllers/Reordering/ANEReorderingVC.h
@@ -0,0 +1,11 @@
+//
+//  ANEReorderingVC.h
+//  Alister-Example
+//
+//  Created by ANODA on 10/20/16.
+//  Copyright © 2016 Oksana Kovalchuk. All rights reserved.
+//
+
+@interface ANEReorderingVC : UIViewController
+
+@end
