dabb91be40d5e363b3458daaf3523c5cc690c90e
Oleg Panfyorov <oleg.panfyorov@anoda.mobi>
2016-10-20 19:09:07 +0300

    ✅ updated tests for ANEDefaultSupplementaryVC

--- Example/Alister-Example-Tests/ExampleTests/ControllersTests/DefaultSupplementary/ANEDefaultSupplementaryVCSpec.m
+++ Example/Alister-Example-Tests/ExampleTests/ControllersTests/DefaultSupplementary/ANEDefaultSupplementaryVCSpec.m
@@ -7,25 +7,68 @@
 //
 
 #import "ANEDefaultSupplementaryVC.h"
+#import "ANTableController.h"
+#import "ANStorage.h"
 
 @interface ANEDefaultSupplementaryVC ()
+
+@property (nonatomic, strong) UITableView* tableView;
+@property (nonatomic, strong) ANTableController* controller;
+@property (nonatomic, strong) ANStorage* storage;
 
 @end
 
 SpecBegin(ANEDefaultSupplementaryVC)
 
-describe(@"Thing", ^{
+describe(@"ANEDefaultSupplementaryVC", ^{
     
     __block ANEDefaultSupplementaryVC* vc = nil;
+    __weak typeof(vc) weakVC = vc;
     
     beforeEach(^{
         vc = [ANEDefaultSupplementaryVC new];
     });
     
-    it(@"should do stuff", ^{
-
+    it(@"should have non nil tableView", ^{
+        expect(vc.tableView).notTo.beNil();
     });
     
+    it(@"table view style should be grouped", ^{
+        expect(vc.tableView.style).equal(UITableViewStyleGrouped);
+    });
+    
+    it(@"should have non nil controller", ^{
+        expect(vc.controller).notTo.beNil();
+    });
+    
+    it(@"should have non nil storage", ^{
+        expect(vc.storage).notTo.beNil();
+    });
+    
+    it(@"storage should have items in section 0 after controller finished update", ^{
+        
+        [vc.controller addUpdatesFinsihedTriggerBlock:^{
+            NSArray* storageItems = [weakVC.storage itemsInSection:0];
+            expect(storageItems).notTo.beNil();
+        }];
+    });
+    
+    it(@"storage should have items in section 1 after controller finished update", ^{
+        
+        [vc.controller addUpdatesFinsihedTriggerBlock:^{
+            NSArray* storageItems = [weakVC.storage itemsInSection:1];
+            expect(storageItems).notTo.beNil();
+        }];
+    });
+    
+    it(@"storage should have items in section 2 after controller finished update", ^{
+        
+        [vc.controller addUpdatesFinsihedTriggerBlock:^{
+            NSArray* storageItems = [weakVC.storage itemsInSection:2];
+            expect(storageItems).notTo.beNil();
+        }];
+    });
+
     afterEach(^{
         vc = nil;
     });
