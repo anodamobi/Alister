671d0e8bc35f88db37737099b7e4f5aaa1f5fb88
Oksana Kovalchuk <oksana@anoda.mobi>
2016-10-20 06:16:20 +0300

    ✅updated tests for ANTableView

---
+++ Example/Alister-Example-Tests/Alister/ANPrototypingUIKit/ANTableViewSpec.m
@@ -0,0 +1,37 @@
+//
+//  ANTableViewTests.m
+//  Alister-Example
+//
+//  Created by Maxim Eremenko on 8/22/16.
+//  Copyright © 2016 Oksana Kovalchuk. All rights reserved.
+//
+
+#import <Alister/ANTableView.h>
+
+SpecBegin(ANTableView)
+
+describe(@"tableViewDefaultStyleWithFrame:style:", ^{
+    
+    it(@"object not nil after creation", ^{
+        ANTableView* table = [ANTableView tableViewDefaultStyleWithFrame:CGRectZero style:UITableViewStylePlain];
+        expect(table).notTo.beNil();
+    });
+});
+
+
+describe(@"addStickyFooter:withFixedHeight:", ^{
+   
+    it(@"if tableview height less than screen height", ^{
+        failure(@"Pending");
+    });
+    
+    it(@"handles navigation bar height", ^{
+        failure(@"Pending");
+    });
+    
+    it(@"handles status bar height", ^{
+        failure(@"Pending");
+    });
+});
+
+SpecEnd
