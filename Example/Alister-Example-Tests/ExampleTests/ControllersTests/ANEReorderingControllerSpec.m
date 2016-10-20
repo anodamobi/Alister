c83be2e6e51a857a17d56eabe4e4f9ef16297d2c
Oleg Panfyorov <oleg.panfyorov@anoda.mobi>
2016-10-20 15:04:31 +0300

    ✅ added reordering vc and controller tests

---
+++ Example/Alister-Example-Tests/ExampleTests/ControllersTests/ANEReorderingControllerSpec.m
@@ -0,0 +1,35 @@
+//
+//  ANEReorderingControllerSpec.m
+//  Alister-Example
+//
+//  Created by ANODA on 10/20/16.
+//  Copyright © 2016 Oksana Kovalchuk. All rights reserved.
+//
+
+#import "ANEReorderingController.h"
+
+SpecBegin(ANEReorderingControllerSpec)
+
+describe(@"Thing", ^{
+    
+    beforeEach(^{
+
+    });
+    
+    it(@"should do stuff", ^{
+
+    });
+    
+    it(@"should do some stuff asynchronously", ^{
+        waitUntil(^(DoneCallback done) {
+
+            done();
+        });
+    });
+    
+    afterEach(^{
+
+    });
+});
+
+SpecEnd
