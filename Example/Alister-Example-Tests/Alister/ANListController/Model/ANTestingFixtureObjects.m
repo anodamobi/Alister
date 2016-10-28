c1564214cd59ac79d628f6bee78ea8c011e0fd05
Oksana Kovalchuk <oksana@anoda.mobi>
2016-10-28 04:03:32 +0300

    🚧 added test fixtures

---
+++ Example/Alister-Example-Tests/Alister/ANListController/Model/ANTestingFixtureObjects.m
@@ -0,0 +1,18 @@
+//
+//  ANTestingFixtureObject.m
+//  Alister-Example
+//
+//  Created by Oksana Kovalchuk on 10/28/16.
+//  Copyright © 2016 Oksana Kovalchuk. All rights reserved.
+//
+
+#import "ANTestingFixtureObjects.h"
+
+@implementation ANTestingFixtureObject
+
+@end
+
+
+@implementation ANTestingFixtureObjectInherited
+
+@end
