44e85110f527d73bb650ab452ffe0a889511dc79
Oksana Kovalchuk <oksana@anoda.mobi>
2016-10-28 05:03:50 +0300

    💄added test helper

---
+++ Example/Alister-Example-Tests/ANTestHelper.h
@@ -0,0 +1,13 @@
+//
+//  ANTestHelper.h
+//  Alister-Example
+//
+//  Created by Oksana Kovalchuk on 10/28/16.
+//  Copyright © 2016 Oksana Kovalchuk. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ANTestHelper : NSObject
+
+@end
---
+++ Example/Alister-Example-Tests/ANTestHelper.m
@@ -0,0 +1,13 @@
+//
+//  ANTestHelper.m
+//  Alister-Example
+//
+//  Created by Oksana Kovalchuk on 10/28/16.
+//  Copyright © 2016 Oksana Kovalchuk. All rights reserved.
+//
+
+#import "ANTestHelper.h"
+
+@implementation ANTestHelper
+
+@end
