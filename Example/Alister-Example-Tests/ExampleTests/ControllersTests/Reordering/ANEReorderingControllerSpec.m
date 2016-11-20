e4a15e0db2e74a3a3862939ecc692ec390b4c95c
Maxim Eremenko <maxim.eremenko@anoda.mobi>
2016-11-20 19:05:05 +0200

    ✅ IOS-141: fixed ANEReorderingController tests

--- Example/Alister-Example-Tests/ExampleTests/ControllersTests/Reordering/ANEReorderingControllerSpec.m
+++ Example/Alister-Example-Tests/ExampleTests/ControllersTests/Reordering/ANEReorderingControllerSpec.m
@@ -13,9 +13,11 @@ SpecBegin(ANEReorderingControllerSpec)
 describe(@"ANEReorderingController", ^{
     
     __block ANEReorderingController* reorderingVC = nil;
+    __block UITableView* tableView = nil;
     
     beforeEach(^{
-        reorderingVC = [[ANEReorderingController alloc] initWithTableView:[UITableView new]];
+        tableView = [UITableView new];
+        reorderingVC = [[ANEReorderingController alloc] initWithTableView:tableView];
     });
     
     it(@"controller should have non nil table view", ^{
