4cece1ec4c202ad2b19287d6f50979c42d46d6cf
Maxim Eremenko <maxim.eremenko@anoda.mobi>
2017-02-07 14:35:11 +0200

    set-up-carthage: added Masonry.framework

---
+++ Example/AlisterIOS/AlisterIOS.h
@@ -0,0 +1,19 @@
+//
+//  AlisterIOS.h
+//  AlisterIOS
+//
+//  Created by ANODA on 2/7/17.
+//  Copyright © 2017 Oksana Kovalchuk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AlisterIOS.
+FOUNDATION_EXPORT double AlisterIOSVersionNumber;
+
+//! Project version string for AlisterIOS.
+FOUNDATION_EXPORT const unsigned char AlisterIOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AlisterIOS/PublicHeader.h>
+
+
