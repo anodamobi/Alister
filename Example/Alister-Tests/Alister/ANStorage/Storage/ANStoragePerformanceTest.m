0b5f5dbb59c5f49ee6a8f9dc7a99a929349514a2
Maxim Eremenko <maxim.eremenko@anoda.mobi>
2017-02-13 17:14:35 +0200

    ✅ an-storage-performance: added performance test for ANStorageUpdatableInterface

--- Example/Alister-Tests/Alister/ANStorage/Storage/ANStoragePerformanceTest.m
+++ Example/Alister-Tests/Alister/ANStorage/Storage/ANStoragePerformanceTest.m
@@ -11,6 +11,7 @@
 #import "ANTableController.h"
 
 static NSUInteger const kGeneratedItemsCount = 10000;
+static NSUInteger const kSectionsCount = 100;
 
 @interface ANStoragePerformanceTest : XCTestCase
 
@@ -25,6 +26,8 @@ static NSUInteger const kGeneratedItemsCount = 10000;
 {
     [super setUp];
     self.storage = [ANStorage new];
+    self.controller = [[ANTableController alloc] initWithTableView:[UITableView new]];
+    [self.controller attachStorage:self.storage];
 }
 
 - (void)tearDown
@@ -37,10 +40,21 @@ static NSUInteger const kGeneratedItemsCount = 10000;
 - (void)test_addItemsToEmptyStoragePerformanceTest
 {
     NSArray* items = [self _generateItems];
+    
+    XCTestExpectation* expectation = [self expectationWithDescription:@"test_addItemsToEmptyStoragePerformanceTest"];
+    
     [self measureBlock:^{
         [self.storage updateWithoutAnimationChangeBlock:^(id<ANStorageUpdatableInterface> storageController) {
-            [storageController addItems:items];
+            [storageController addItems:items toSection:0];
         }];
+        
+        [self.controller addUpdatesFinishedTriggerBlock:^{
+            [expectation fulfill];
+        }];
+    }];
+    
+    [self waitForExpectationsWithTimeout:1.5f handler:^(NSError* _Nullable error) {
+        expect(error).to.beNil();
     }];
 }
 
@@ -48,37 +62,184 @@ static NSUInteger const kGeneratedItemsCount = 10000;
 {
     //given: fill storage
     [self.storage updateWithoutAnimationChangeBlock:^(id<ANStorageUpdatableInterface> storageController) {
-        NSUInteger sectionsCount = 100;
-        for (NSUInteger sectionIndex = 0; sectionIndex < sectionsCount; sectionIndex++)
+        for (NSUInteger sectionIndex = 0; sectionIndex < kSectionsCount; sectionIndex++)
         {
             NSArray* items = [self _generateItems];
             [storageController addItems:items toSection:sectionIndex];
         }
     }];
     
-    self.controller = [[ANTableController alloc] initWithTableView:[UITableView new]];
-    [self.controller attachStorage:self.storage];
-    XCTestExpectation* expectation = [self expectationWithDescription:@""];
+    XCTestExpectation* expectation = [self expectationWithDescription:@"test_insertSingleItemToFilledStoragePerformanceTest"];
+    NSUInteger midleSectionIndex = kSectionsCount / 2;
+    NSArray* items = [self _generateItems];
     
-    __weak typeof(self) welf = self;
-    static BOOL isFirstInsert = YES;
-    [self.controller addUpdatesFinishedTriggerBlock:^{
-        if (isFirstInsert)
+    //then
+    [self measureBlock:^{
+        [self.storage updateWithoutAnimationChangeBlock:^(id<ANStorageUpdatableInterface> storageController) {
+            [storageController addItems:items toSection:midleSectionIndex];
+        }];
+        
+        [self.controller addUpdatesFinishedTriggerBlock:^{
+            [expectation fulfill];
+        }];
+    }];
+    
+    [self waitForExpectationsWithTimeout:1.5f handler:^(NSError* _Nullable error) {
+        expect(error).to.beNil();
+    }];
+}
+
+- (void)test_removeItemsInFilledStoragePerformanceTest
+{
+    //given: fill storage
+    [self.storage updateWithoutAnimationChangeBlock:^(id<ANStorageUpdatableInterface> storageController) {
+        for (NSUInteger sectionIndex = 0; sectionIndex < kSectionsCount; sectionIndex++)
         {
-            isFirstInsert = NO;
-            [welf measureBlock:^{
-                [welf.storage updateWithoutAnimationChangeBlock:^(id<ANStorageUpdatableInterface> storageController) {
-                    
-                    NSUInteger lastIndex = kGeneratedItemsCount - 1;
-                    [storageController addItem:@"TestItem" toSection:lastIndex];
-                    
-                    [expectation fulfill];
-                }];
-            }];
+            NSArray* items = [self _generateItems];
+            [storageController addItems:items toSection:sectionIndex];
         }
     }];
     
-    [self waitForExpectationsWithTimeout:2 handler:^(NSError * _Nullable error) {
+    NSIndexPath* indexPath = [self _middleIndexPath];
+    XCTestExpectation* expectation = [self expectationWithDescription:@"test_removeItemsInFilledStoragePerformanceTest"];
+    
+    //then
+    [self measureBlock:^{
+        [self.storage updateWithoutAnimationChangeBlock:^(id<ANStorageUpdatableInterface> storageController) {
+            [storageController removeItemsAtIndexPaths:@[indexPath]];
+        }];
+        
+        [self.controller addUpdatesFinishedTriggerBlock:^{
+            [expectation fulfill];
+        }];
+    }];
+    
+    [self waitForExpectationsWithTimeout:1.5f handler:^(NSError* _Nullable error) {
+        expect(error).to.beNil();
+    }];
+}
+
+- (void)test_removeSectionsInFilledStoragePerformanceTest
+{
+    //given: fill storage
+    [self.storage updateWithoutAnimationChangeBlock:^(id<ANStorageUpdatableInterface> storageController) {
+        for (NSUInteger sectionIndex = 0; sectionIndex < kSectionsCount; sectionIndex++)
+        {
+            NSArray* items = [self _generateItems];
+            [storageController addItems:items toSection:sectionIndex];
+        }
+    }];
+    
+    NSIndexSet* indexSet = [NSIndexSet indexSetWithIndexesInRange:NSMakeRange(0, kSectionsCount - 1)];
+    XCTestExpectation* expectation = [self expectationWithDescription:@"test_removeSectionsInFilledStoragePerformanceTest"];
+    
+    //then
+    [self measureBlock:^{
+        [self.storage updateWithoutAnimationChangeBlock:^(id<ANStorageUpdatableInterface> storageController) {
+            [storageController removeSections:indexSet];
+        }];
+        
+        [self.controller addUpdatesFinishedTriggerBlock:^{
+            [expectation fulfill];
+        }];
+    }];
+    
+    [self waitForExpectationsWithTimeout:1.5f handler:^(NSError* _Nullable error) {
+        expect(error).to.beNil();
+    }];
+}
+
+- (void)test_replaceItemWithItemPerformanceTest
+{
+    //given: fill storage
+    [self.storage updateWithoutAnimationChangeBlock:^(id<ANStorageUpdatableInterface> storageController) {
+        for (NSUInteger sectionIndex = 0; sectionIndex < kSectionsCount; sectionIndex++)
+        {
+            NSArray* items = [self _generateItems];
+            [storageController addItems:items toSection:sectionIndex];
+        }
+    }];
+    
+    NSIndexPath* indexPath = [self _middleIndexPath];
+    XCTestExpectation* expectation = [self expectationWithDescription:@"test_replaceItemWithItemPerformanceTest"];
+    
+    NSString* oldItem = [self.storage objectAtIndexPath:indexPath];
+    NSString* updatedItem = @"";
+    
+    //then
+    [self measureBlock:^{
+        [self.storage updateWithoutAnimationChangeBlock:^(id<ANStorageUpdatableInterface> storageController) {
+            [storageController replaceItem:oldItem withItem:updatedItem];
+        }];
+        
+        [self.controller addUpdatesFinishedTriggerBlock:^{
+            [expectation fulfill];
+        }];
+    }];
+    
+    [self waitForExpectationsWithTimeout:1.5f handler:^(NSError* _Nullable error) {
+        expect(error).to.beNil();
+    }];
+}
+
+- (void)test_moveItemWithoutUpdateFromIndexPathInFilledStoragePerformanceTest
+{
+    //given: fill storage
+    
+    [self.storage updateWithoutAnimationChangeBlock:^(id<ANStorageUpdatableInterface> storageController) {
+        for (NSUInteger sectionIndex = 0; sectionIndex < kSectionsCount; sectionIndex++)
+        {
+            NSArray* items = [self _generateItems];
+            [storageController addItems:items toSection:sectionIndex];
+        }
+    }];
+    
+    NSIndexPath* fromIndexPath = [self _middleIndexPath];
+    NSIndexPath* toIndexPath = [NSIndexPath indexPathForRow:0 inSection:kSectionsCount - 1];
+    XCTestExpectation* expectation = [self expectationWithDescription:@"test_replaceItemWithItemPerformanceTest"];
+    
+    //then
+    [self measureBlock:^{
+        [self.storage updateWithoutAnimationChangeBlock:^(id<ANStorageUpdatableInterface> storageController) {
+            [storageController moveItemFromIndexPath:fromIndexPath toIndexPath:toIndexPath];
+        }];
+        
+        [self.controller addUpdatesFinishedTriggerBlock:^{
+            [expectation fulfill];
+        }];
+    }];
+    
+    [self waitForExpectationsWithTimeout:1.5f handler:^(NSError* _Nullable error) {
+        expect(error).to.beNil();
+    }];
+}
+
+- (void)test_reloadItemsInFilledStoragePerformanceTest
+{
+    //given: fill storage
+    __block NSArray* items  = nil;
+    [self.storage updateWithoutAnimationChangeBlock:^(id<ANStorageUpdatableInterface> storageController) {
+        for (NSUInteger sectionIndex = 0; sectionIndex < kSectionsCount; sectionIndex++)
+        {
+            items = [self _generateItems];
+            [storageController addItems:items toSection:sectionIndex];
+        }
+    }];
+    
+    XCTestExpectation* expectation = [self expectationWithDescription:@"test_reloadItemsInFilledStoragePerformanceTest"];
+    
+    //then
+    [self measureBlock:^{
+        [self.storage updateWithoutAnimationChangeBlock:^(id<ANStorageUpdatableInterface> storageController) {
+            [storageController reloadItems:items];
+        }];
+        
+        [self.controller addUpdatesFinishedTriggerBlock:^{
+            [expectation fulfill];
+        }];
+    }];
+    
+    [self waitForExpectationsWithTimeout:20 handler:^(NSError* _Nullable error) {
         expect(error).to.beNil();
     }];
 }
@@ -93,7 +254,16 @@ static NSUInteger const kGeneratedItemsCount = 10000;
     {
         [items addObject:@"TestObject"];
     }
+    
     return items;
+}
+
+- (NSIndexPath*)_middleIndexPath
+{
+    NSUInteger sectionsCount = 100;
+    NSUInteger midleSectionIndex = sectionsCount / 2;
+    
+    return [NSIndexPath indexPathForRow:kGeneratedItemsCount - 1 inSection:midleSectionIndex];
 }
 
 @end
